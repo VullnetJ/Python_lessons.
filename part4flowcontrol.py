1944f3744576d10476374ab09641ac4d33e26030
VullnetJ <46821353+VullnetJ@users.noreply.github.com>
2019-03-21 15:15:37 +0200

    Add files via upload

---
+++ part4flowcontrol.py
@@ -0,0 +1,26 @@
+# Tiki explained for: Flow control,  if statements and while loop, how they work 
+# Relational operators are six: ==same as, != not same as, >more than, <less than, <=less than or equal, >=more than or equal. 
+# in python is resolved left to write. 
+""" Logical Operators: and, or not. 
+ tab is important in python.
+
+"""
+x = 50
+y = 11
+if x != y and (x <10 or y > 100): 
+    print('Thank you')
+elif x >0:
+    print("I don't know")
+    print("Blah!")
+    if x==50:
+        print("hahhaha") # is part of elif (it is nested if)
+# elif has to be between if and else.
+    elif x  == 100: 
+        print("Wow! that's big") 
+else: 
+    print("Ei kiitos")
+
+#python doesnt have a switch statment like other programming languages.  
+# short for else if. 
+
+# nest week we will do the loops. Congratulations you survived part4. 
